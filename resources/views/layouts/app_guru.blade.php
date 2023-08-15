f905ee6899c37d0912f29ad5e18f3ab1cfb0fbc3
ramli <rizalramli@its.ac.id>
2023-08-15 11:06:18 +0700

    update footer

--- resources/views/layouts/app_guru.blade.php
+++ resources/views/layouts/app_guru.blade.php
@@ -20,11 +20,7 @@
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-start">
-                        <p>2021 &copy; Mazer</p>
-                    </div>
-                    <div class="float-end">
-                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                                href="https://saugi.me">Saugi</a></p>
+                        <p>2023 &copy; MA MIFTAHUL ULUM PANDANWANGI</p>
                     </div>
                 </div>
             </footer>
