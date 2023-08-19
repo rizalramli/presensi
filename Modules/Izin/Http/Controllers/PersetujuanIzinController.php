49684072de668444eb288e17a53b5958b80ece5b
ramli <rizalramli@its.ac.id>
2023-08-19 22:56:57 +0700

    Implement database persetujuan izin

--- Modules/Izin/Http/Controllers/PersetujuanIzinController.php
+++ Modules/Izin/Http/Controllers/PersetujuanIzinController.php
@@ -2,89 +2,32 @@
 
 namespace Modules\Izin\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Http\Request;
+use App\Models\PersetujuanIzin;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class PersetujuanIzinController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
+
     public function index()
     {
         $daftar_bulan = ['', 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
-        $daftar_guru = [
-            (object)[
-                'id_guru' => 1,
-                'nama' => 'Rizal Ramli'
-            ],
-            (object)[
-                'id_guru' => 1,
-                'nama' => 'Hasri Wiji'
-            ]
-        ];
+        $daftar_guru = PersetujuanIzin::getDataGuru();
+        if (request()->ajax()) {
+            $bulan = request()->bulan;
+            $tahun = request()->tahun;
+            $id_user = request()->id_user;
+            $data = PersetujuanIzin::getData($bulan, $tahun, $id_user);
+            return response()->json($data);
+        }
         return view('izin::persetujuan_izin.index', compact('daftar_bulan', 'daftar_guru'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
-    public function create()
+    public function store()
     {
-        return view('cuti::create');
-    }
+        request()->id_user_konfirmasi = Auth::id();
 
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function show($id)
-    {
-        return view('cuti::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        return view('cuti::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
-    public function destroy($id)
-    {
-        //
+        $data = PersetujuanIzin::simpanData(request());
+        return Response()->json($data);
     }
 }
---
+++ app/Models/PersetujuanIzin.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class PersetujuanIzin extends Model
+{
+    use HasFactory;
+
+    public $table = 'izin';
+
+    public $timestamps = true;
+
+    public $fillable = [
+        'id_jenis_izin',
+        'id_user',
+        'id_user_konfirmasi',
+        'tanggal',
+        'dari_jam',
+        'sampai_jam',
+        'bukti_foto',
+        'keterangan',
+        'tanggal_konfirmasi',
+        'alasan_penolakan',
+        'status'
+    ];
+
+    public static function getDataGuru()
+    {
+        $sql = "SELECT id,name as nama FROM users";
+
+        $sql = $sql . " ORDER BY name ASC";
+
+        $result = DB::select($sql);
+
+        return $result;
+    }
+
+    public static function getData($bulan, $tahun, $id_user)
+    {
+        $sql = "
+        SELECT 
+            i.id,
+            ji.nama as jenis_izin,
+            u.name as nama,
+            i.tanggal,
+            i.dari_jam,
+            i.sampai_jam,
+            i.bukti_foto,
+            i.keterangan,
+            i.alasan_penolakan,
+            i.status
+        FROM izin i
+        JOIN 
+            jenis_izin ji ON ji.id = i.id_jenis_izin
+        JOIN 
+            users u ON u.id = i.id_user
+        WHERE 
+            i.deleted_at IS NULL
+        AND 
+            i.status = 0 
+        AND 
+            MONTH(i.tanggal) = :bulan
+        AND 
+            YEAR(i.tanggal) = :tahun";
+
+        if ($id_user != null) {
+            $sql .= " AND i.id_user = " . $id_user;
+        }
+
+        $sql = $sql . " ORDER BY i.tanggal DESC,i.id DESC";
+
+        $result = DB::select($sql, [
+            'bulan' => $bulan,
+            'tahun' => $tahun
+        ]);
+
+        return $result;
+    }
+
+    public static function simpanData($request)
+    {
+        $sql = DB::table('izin')
+            ->where('id', $request->id)
+            ->update([
+                'id_user_konfirmasi' => $request->id_user_konfirmasi,
+                'tanggal_konfirmasi' => now(),
+                'alasan_penolakan' => $request->alasan_penolakan,
+                'status' => $request->status,
+                'updated_at' => now()
+            ]);
+
+        return $sql;
+    }
+}
